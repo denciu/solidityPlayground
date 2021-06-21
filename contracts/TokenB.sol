7f9248ec20bdd41b0170bcfc1961a7adfe2b4db2
Daniel Cybulka <daniel.cybulka@rumblefish.pl>
2021-06-21 10:45:20 +0200

    added faucet to tokens

--- contracts/TokenB.sol
+++ contracts/TokenB.sol
@@ -7,6 +7,10 @@ contract TokenB is ERC20PresetMinterPauser {
     constructor(string memory _name, string memory _symbol)
         ERC20PresetMinterPauser(_name, _symbol)
     {
+        _mint(msg.sender, 10 ether);
+    }
+
+    function faucet() public {
         _mint(msg.sender, 1 ether);
     }
 }
